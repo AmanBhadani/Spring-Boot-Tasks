8ad1b4055fc3e38fa374f8604b60d17de9b2439c
AmanBhadani <aman.bhadani.01@gmail.com>
2019-10-17 10:26:30 +0530

    mysql

--- src/main/java/com/stackroute/muzixapp/startup/ApplicationListen.java
+++ src/main/java/com/stackroute/muzixapp/startup/ApplicationListen.java
@@ -2,26 +2,28 @@ package com.stackroute.muzixapp.startup;
 
 import com.stackroute.muzixapp.domain.Track;
 import com.stackroute.muzixapp.repository.TrackRepository;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
-
 @ConfigurationProperties
-@Component
 public class ApplicationListen implements ApplicationListener<ContextRefreshedEvent> {
-    @Autowired
     private TrackRepository trackRepository;
 
     @Autowired
-    Environment environment;
+    public void SetTrackRepository(TrackRepository trackRepository){
+        this.trackRepository=trackRepository;
+    }
+
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        System.out.println("inside listener");
-        trackRepository.save(new Track(Integer.parseInt(environment.getProperty("id2")), environment.getProperty("name2"), environment.getProperty("comment2")));
+
+        Track track= new Track();
+        track.setId(14);
+        track.setComment("Bhadani");
+        track.setName("Aman");
+        trackRepository.save(track);
+
     }
 }
