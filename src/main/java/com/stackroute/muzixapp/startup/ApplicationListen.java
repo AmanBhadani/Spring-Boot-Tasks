b614b6f880f327d3757d0f494a769cfcf4f32ea1
AmanBhadani <aman.bhadani.01@gmail.com>
2019-10-17 10:21:01 +0530

    mysql

--- src/main/java/com/stackroute/muzixapp/startup/ApplicationListen.java
+++ src/main/java/com/stackroute/muzixapp/startup/ApplicationListen.java
@@ -2,38 +2,26 @@ package com.stackroute.muzixapp.startup;
 
 import com.stackroute.muzixapp.domain.Track;
 import com.stackroute.muzixapp.repository.TrackRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-
-@PropertySource("classpath:application.properties")
+@ConfigurationProperties
+@Component
 public class ApplicationListen implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
     private TrackRepository trackRepository;
 
-    @Value("$(track.id1)")
-    private int id;
-
     @Autowired
-    private Environment environment;
-
-    @Autowired
-    public void SetTrackRepository(TrackRepository trackRepository){
-        this.trackRepository=trackRepository;
-    }
-
+    Environment environment;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
-        Track track= new Track();
-        track.setId(id);
-        track.setComment(environment.getProperty("track.comment1"));
-        track.setName(environment.getProperty("track.name1"));
-        trackRepository.save(track);
-
+        System.out.println("inside listener");
+        trackRepository.save(new Track(Integer.parseInt(environment.getProperty("id2")), environment.getProperty("name2"), environment.getProperty("comment2")));
     }
 }
